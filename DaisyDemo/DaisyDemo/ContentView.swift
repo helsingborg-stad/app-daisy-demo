92cee57b6c13aa36983cc132f4f5f9f2e2fba5f8
Ehsan Zilaei <zzilaei@gmail.com>
2021-09-22 10:24:26 +0200

    initial commit

---
+++ DaisyDemo/DaisyDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DaisyDemo
+//
+//  Created by Ehsan Zilaei on 2021-09-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DaisyDemo/DaisyDemo/DaisyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaisyDemoApp.swift
+//  DaisyDemo
+//
+//  Created by Ehsan Zilaei on 2021-09-22.
+//
+
+import SwiftUI
+
+@main
+struct DaisyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
