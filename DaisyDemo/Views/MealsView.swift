55d978881265558981dfa3f5babeccb44ac0700c
zilaei <zzilaei@gmail.com>
2021-10-05 12:00:04 +0200

    feat(meals): added meals to demo

---
+++ DaisyDemo/Views/MealsView.swift
@@ -0,0 +1,22 @@
+//
+//  MealsView.swift
+//  DaisyDemo
+//
+//  Created by Ehsan Zilaei on 2021-10-05.
+//
+
+import SwiftUI
+import Combine
+import Meals
+
+struct MealsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MealsView_Previews: PreviewProvider {
+    static var previews: some View {
+        MealsView()
+    }
+}
